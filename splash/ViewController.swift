088370da9e804e0713bed8fd80d132cfc3c71a91
Sameer Bhati <bhati.sameer987@gmail.com>
2025-07-16 11:32:58 +0530

    inital commit

--- splash/ViewController.swift
+++ splash/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import SwiftyGif
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgVideo: UIImageView!
+    @IBOutlet weak var splashView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playGIF()
     }
+    
+    func playGIF() {
+        do {
+            let gif = try UIImage(gifName: "sorry.gif")
+            let gifManager = SwiftyGifManager(memoryLimit: 20)
+            imgVideo.setGifImage(gif, manager: gifManager, loopCount: -1)
+        } catch {
+            print("GIF not found or failed to load")
+        }
 
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            //LocationManager.sharedInstance.startUpdatingLocation()
+            let vc = self.storyboard?.instantiateViewController(identifier: "HomeVC") as! HomeVC
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
